5d1e10ee81178a31301425a2435974b49846ba1a
ponbiki <ponbiki@beryllium>
2015-08-25 20:23:31 -0400

    Pulled jquery mobile and converted to bootstrap

--- app/routes/home.php
+++ app/routes/home.php
@@ -3,11 +3,10 @@
 use ponbiki\radio as rad;
 
 $app->get('/', function () use ($app) {
-    
-    
+
     $page = "7chan Radio";
     $meta = "Main Page";
-    
+
     $app->render('home.html.twig', [
         'page' => $page,
         'meta' => $meta
