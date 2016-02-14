9ff3211ccfffc465dd264ee7a569b46f5929fe5b
ponbiki <jcandroscavage@hotmail.com>
2016-02-14 02:17:36 -0500

    phpdoc'ed meta interface

--- src/iMeta.php
+++ src/iMeta.php
@@ -3,8 +3,18 @@
 
 namespace ponbiki\radio;
 
+/**
+ * Remotely grab, parse, and return stream metadata for asynchronous updating of "Now Playing"
+ * Class Meta
+ * @package ponbiki\radio
+ * @license GPLv2
+ */
 
 interface iMeta
 {
+    /**
+     * Trigger new poll of the stream data and return all meta to the app if available or report off state
+     * @return array $this->meta Returns assoc array of all stream metadata
+     */
     public function getMeta();
 }
