791e8eec35ef0e936e58645a95b2e49cec8fe9c0
ponbiki <jcandroscavage@hotmail.com>
2016-02-17 01:41:06 -0500

    Some HTML changes, and on response in meta route

--- app/routes/meta.php
+++ app/routes/meta.php
@@ -4,10 +4,23 @@ use ponbiki\radio as radio;
 
 $app->get('/meta', function () use ($app) {
 
-    print("<ul>");
-    foreach ((new radio\Meta())->getMeta() as $key => $value) {
-        print("<li>$key -- $value </li>");
-     }
-    print("</ul>");
+    /**
+     * @var array Loads the results of "now playin" meta into a local array
+     */
+    $metaArray = (new radio\Meta())->getMeta();
+
+    /**
+     * Checks status and returns meta or off error items
+     * @return mixed Returns messages as li
+     */
+    if ($metaArray['status'] === "off") {
+        print("<li>&#9785; OFF AIR &#9785;</li>");
+    } elseif ($metaArray['status'] === "on") {
+        print("<li>DJ: {$metaArray['dj']}</li>");
+        print("<li>Playing: {$metaArray['playing']}</li>");
+        print("<li>Listeners: {$metaArray['listeners']}</li>");
+    } else {
+        print("Your mama so fat, she caused a server error!");
+    }
 
 });
