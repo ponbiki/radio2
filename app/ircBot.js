d41f451c25fb5a90aa5134d37480a5a0ca40cd6f
ponbiki <jcandroscavage@hotmail.com>
2016-04-13 02:36:28 -0400

    connection details

--- app/ircBot.js
+++ app/ircBot.js
@@ -4,6 +4,7 @@ var config = {
     botName: "d[^_^]b",
     userName: "techno_viking",
     realName: "T-1000",
+    autoConnect: false,
     autoRejoin: true
 };
 
@@ -12,5 +13,17 @@ var irc = require("irc");
 var bot = new irc.Client(config.server, config.botName, {
     userName: config.userName,
     realName: config.realName,
-    channels: config.channels
+    channels: config.channels,
+    autoConnect: config.autoConnect,
+    autoRejoin: config.autoRejoin
+});
+
+bot.connect(5, function(input) {
+    console.log("Connected to!");
+    for (var channel in config.channels) {
+        bot.join(config.channels, function(input) {
+            console.log("Joined #radio");
+            bot.say('#radio', "I like hipster music!");
+        });
+    }
 });
